ad327570d5a055630b51c2657fd2ccb04a0eb875
Ahmed Magdy ELzayady <91629766+Ahmed-Elzayady@users.noreply.github.com>
2023-01-28 13:25:55 +0200

    update

--- js/script.js
+++ js/script.js
@@ -2,17 +2,17 @@ icon = document.querySelector(".icon");
 icon.onclick = function () {
   navBar = document.querySelector(".bar");
   navBar.classList.toggle("active");
-}
+};
 
 let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
@@ -20,8 +20,12 @@ function showSlides(n) {
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("demo");
   let captionText = document.getElementById("caption");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -32,23 +36,7 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
   captionText.innerHTML = dots[slideIndex - 1].alt;
 }
-
-
-'use strict';
-
-// function typeWriter(an) {
-//   const textArray = an.innerHTML.split('');
-//   an.innerHTML = '';
-//   textArray.forEach((letter, a) =>
-//     setTimeout(() => (an.innerHTML += letter), 95 * a)
-//   );
-//   setInterval(() => typeWriter(an), 8000);
-// }
-
-// typeWriter(animation);
-
-
-
+("use strict");
 var loader = document.getElementById("loaderr");
 window.addEventListener("load", function () {
   loader.style.display = "none";
